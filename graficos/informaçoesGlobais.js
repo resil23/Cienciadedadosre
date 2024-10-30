ad789b5379ff31a50855fa34ed7e3ca80029624e
resil23 <128328104+resil23@users.noreply.github.com>
2024-10-30 19:28:56 -0300

    .

--- graficos/informaçoesGlobais.js
+++ graficos/informaçoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span> ${dados.total_pessoas_mundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span> ${dados.total_pessoas_com_acesso_a_educacao}</span>? O tempo médio gasto por dia estudando é de <span> ${dados.tempo_medio_dia_estudando}</span> horas e o total de pessoas com educação superior é de <span> ${dados.total_pessoas_com_educacao_superior}.</span>  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
+    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${dados.total_pessoas_mundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${dados.total_pessoas_com_acesso_a_educacao}</span>? O tempo médio gasto por dia estudando é de <span>${dados.tempo_medio_dia_estudando}</span> horas e o total de pessoas com educação superior é de <span>${dados.total_pessoas_com_educacao_superior}</span>.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
 `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
