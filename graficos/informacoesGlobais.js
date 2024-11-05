0436a9e726f11a8594db831ab6e7abf7385b4971
resil23 <128328104+resil23@users.noreply.github.com>
2024-11-05 20:16:50 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,10 +8,11 @@ async function vizualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 60)
     const pessoasComEducacaoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
+    const percentual = ((pessoasComAcessoAEducacao/pessoasMundo)*100).toFixed(2)
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${pessoasMundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span>? O tempo médio gasto por dia estudando é de <span>${horas}</span> horas e <span>${minutos}</span> minutos e o total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}</span>.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
+    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${pessoasMundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span>? O tempo médio gasto por dia estudando é de <span>${horas}</span> horas e <span>${minutos}</span> minutos e o total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}</span>.Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação.  
 `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
